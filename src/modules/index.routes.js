39e514d45a2a3fae0e5bb1be99138fa76f2dcf6c
Youssef_Abdo <96637360+yousef21321@users.noreply.github.com>
2024-04-24 21:23:47 +0200

    Update index.routes.js

--- src/modules/index.routes.js
+++ src/modules/index.routes.js
@@ -1,10 +1,8 @@
 import { globalError } from "../middleware/gobalErrorMiddleware.js"
 import userRouter from "./User/user.routes.js"
 import biddeeRouter from "./bidders/bidders.routes.js"
-import cartRouter from "./cart/cart.routes.js"
 import categoryRouter from "./category/category.routes.js"
 import favoritepageRouter from "./favoritepage/favoritepage.routes.js"
-import orderRouter from "./order/order.routes.js"
 import productRouter from "./product/product.routes.js"
 import subcategoryRouter from "./subcategory/subcategory.routes.js"
 import technicalRouter from "./technical/technical.routes.js"
@@ -18,8 +16,6 @@ export const bootstap = (app)=>{
     app.use('/api/v1/products',productRouter)
     app.use("/api/v1/users",userRouter);
     app.use("/api/v1/technical",technicalRouter);
-    app.use("/api/v1/carts",cartRouter);
-    app.use("/api/v1/orders",orderRouter);
     app.use("/api/v1/bidders",biddeeRouter);
     app.use("/api/v1/favoritepage",favoritepageRouter);
 
